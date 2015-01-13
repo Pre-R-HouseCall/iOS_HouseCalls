826d1c64dc1826822a41ab9e980ce928207a4f93
timacorda <tacorda@calpoly.edu>
2015-01-13 10:23:03 -0800

    iOS Project

---
+++ House_calls/House_calls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  House_calls
+//
+//  Created by Timothy Acorda on 1/13/15.
+//  Copyright (c) 2015 Timothy Acorda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ House_calls/House_calls/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  House_calls
+//
+//  Created by Timothy Acorda on 1/13/15.
+//  Copyright (c) 2015 Timothy Acorda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
